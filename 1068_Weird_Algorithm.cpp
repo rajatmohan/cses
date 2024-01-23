af01ebcbd47386dfcf6b3083c1e1ec0e6deed45f
rajatmohan <rajatm2196@gmail.com>
2024-01-23 22:19:37 +0530

    Do 1068_Weird_Algorithm problem

---
+++ 1068_Weird_Algorithm.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	cin.sync_with_stdio(false);
+    long n;
+    cin >> n;
+	while(n > 1) {
+        cout << n << " ";
+        if(n % 2 == 0) {
+            n = n / 2;
+        }
+        else {
+            n = 3*n + 1;
+        }
+    }
+    cout << 1 << endl;
+}
